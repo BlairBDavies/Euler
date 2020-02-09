8b6c0aea5f4afbfc1c8f5116b003553cbaf9912e
Blair Davies <blair.davies@gmail.com>
2020-02-08 22:07:09 -0800

    Problem 1 and unit tests

--- Euler/Problem1.cs
+++ Euler/Problem1.cs
@@ -1,8 +1,28 @@
 ﻿using System;
+using System.Linq;
 
 namespace Euler
 {
     public class Problem1
     {
+        /* 
+        
+        If we list all the natural numbers below 10
+        that are multiples of 3 or 5, we get 3, 5, 
+        6 and 9. The sum of these multiples is 23.
+
+        Find the sum of all the multiples of 3 or 5 below 1000.
+
+        */
+
+        public static int SumOfMultiples(int maximum = 1000)
+        {
+            if (maximum < 3) return 0;
+
+            maximum--;
+
+            return Enumerable.Range(1, maximum / 3).Select(x => x * 3).Union(
+                Enumerable.Range(1, maximum / 5).Select(x => x * 5)).Distinct().Sum();
+        }
     }
 }
--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -13,10 +13,21 @@ namespace Test
         [Test]
         public void Test1()
         {
-            var problem = new Problem1();
+            //Development confirmation
+            Assert.AreEqual(0, Problem1.SumOfMultiples(3));
+            Assert.AreEqual(3, Problem1.SumOfMultiples(4));
+            Assert.AreEqual(3, Problem1.SumOfMultiples(5));
+            Assert.AreEqual(8, Problem1.SumOfMultiples(6));
+            Assert.AreEqual(14, Problem1.SumOfMultiples(7));
+            Assert.AreEqual(14, Problem1.SumOfMultiples(9));
+            Assert.AreEqual(23, Problem1.SumOfMultiples(10));
+        }
 
-             
-
+        [Test]
+        public void Test2()
+        {
+            //Problem confirmation
+            Assert.AreEqual(233168, Problem1.SumOfMultiples(1000));
         }
     }
 }
